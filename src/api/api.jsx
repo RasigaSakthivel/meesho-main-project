b17ec560ec323a60331e6144288b22293600cacd
RasigaSakthivel <146985115+RasigaSakthivel@users.noreply.github.com>
2024-11-15 13:58:54 +0530

    Add files via upload

---
+++ src/api/api.jsx
@@ -0,0 +1,9 @@
+//This file contains the api link of products
+import axios from "axios";
+
+export async function productsData(){
+    const products = await axios.get(
+    "https://fakestoreapi.com/products"
+   );       
+    return products;
+}
